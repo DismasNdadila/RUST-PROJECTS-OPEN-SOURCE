7f8c6a7a0313fb1203a5e035fadffa46afafe30f
Dismas Ndadila <ndadilladismas@gmail.com>
2025-03-10 02:48:10 +0300

    CLI calculators simple and Complex

--- cli_calculator/src/main.rs
+++ cli_calculator/src/main.rs
@@ -1,6 +1,28 @@
 use std::io;
 
 fn main() {
+
+    println!("Welcome to Rust CLI Calculator by @DismasNdadila");
+    println!("Enter 1 for Simple Calculator");
+    println!("Enter 2 for Advanced Calculator");
+
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
+    let choice: i32 = choice.trim().parse().expect("Please type a number!");
+
+    match choice {
+        1 => simple_calculator(),
+        2 => advanced_calculator(),
+        _ => println!("Invalid choice"),
+    }
+ 
+}
+
+fn simple_calculator(){
+   
     println!("Enter the first number: ");
 
     let mut number1 = String::new();
@@ -42,4 +64,65 @@ fn main() {
     };
 
     println!("You wanted to perform {} {} {} = {}", number1, operation, number2, result);
+}
+
+fn advanced_calculator(){
+
+ println!("Rust CLI Calculator");
+    println!("Enter numbers followed by an operator (e.g., '+', '-', '*', '/').");
+    println!("Type '=' to calculate the result.");
+    println!("Type 'exit' to quit.");
+
+    loop {
+        let mut numbers: Vec<f64> = Vec::new(); 
+        let mut operator = String::new();
+
+        println!("Enter numbers (type '=' to calculate):");
+
+  
+        loop {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read input");
+
+            let trimmed = input.trim();
+
+            if trimmed == "=" { break; } 
+            if trimmed == "exit" { return; } 
+
+            match trimmed.parse::<f64>() {
+                Ok(num) => numbers.push(num),
+                Err(_) => {
+                    println!("Invalid number! Please enter a valid number.");
+                }
+            }
+        }
+
+        println!("Enter operator (+, -, *, /):");
+        io::stdin().read_line(&mut operator).expect("Failed to read input");
+        let operator = operator.trim();
+
+        let mut result = numbers[0]; 
+
+        for &num in &numbers[1..] {
+            result = match operator {
+                "+" => result + num,
+                "-" => result - num,
+                "*" => result * num,
+                "/" => {
+                    if num != 0.0 {
+                        result / num
+                    } else {
+                        println!("Error: Cannot divide by zero.");
+                        return;
+                    }
+                }
+                _ => {
+                    println!("Invalid operator.");
+                    return;
+                }
+            };
+        }
+
+        println!("Result: {}", result);
+    }
 }
