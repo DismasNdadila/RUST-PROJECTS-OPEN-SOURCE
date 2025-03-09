47f91fff78a0b0044464850f8fc4643534465e51
Dismas Ndadila <ndadilladismas@gmail.com>
2025-03-10 00:56:37 +0300

    another hello world

---
+++ cli_calculator/target/debug/cli_calculator.d
@@ -0,0 +1,1 @@
+/home/dismasndadila/Documents/RUST\ PROJECTS\ OPEN\ SOURCE/cli_calculator/target/debug/cli_calculator: /home/dismasndadila/Documents/RUST\ PROJECTS\ OPEN\ SOURCE/cli_calculator/src/main.rs
---
+++ cli_calculator/target/debug/deps/cli_calculator-5f611f7509ebcadd.d
@@ -0,0 +1,5 @@
+/home/dismasndadila/Documents/RUST PROJECTS OPEN SOURCE/cli_calculator/target/debug/deps/cli_calculator-5f611f7509ebcadd: src/main.rs
+
+/home/dismasndadila/Documents/RUST PROJECTS OPEN SOURCE/cli_calculator/target/debug/deps/cli_calculator-5f611f7509ebcadd.d: src/main.rs
+
+src/main.rs:
